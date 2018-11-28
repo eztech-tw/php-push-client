cf5aad4c3e7105cefe3637385ba395f7021e2acb
Scott1984 <scott1984@gmail.com>
2018-11-28 11:45:19 +0800

    改為靜態方法，加入CURL引用

--- EzPush.php
+++ EzPush.php
@@ -10,22 +10,11 @@ class EzPush
     /**
      * @var string 推播伺服器位址
      */
-    public $ServerAddress = "https://example.com/push/v1/";
+    public static $ServerAddress = "https://example.com/push/v1/";
     /**
      * @var string 存取權杖，可於推播伺服器產生
      */
-    public $ApiAccessKey = "ApiAccessKey";
-
-    /**
-     * EzPush constructor.
-     * @param string $_ServerAddress 推播伺服器位址
-     * @param string $_ApiAccessKey 存取權杖，可於推播伺服器產生
-     */
-    function __construct($_ServerAddress,$_ApiAccessKey)
-    {
-        $ServerAddress = $_ServerAddress;
-        $ApiAccessKey = $_ApiAccessKey;
-    }
+    public static $ApiAccessKey = "ApiAccessKey";
 
     /**
      * 發送推播，請先建立一個 Message 物件來傳送
@@ -33,7 +22,7 @@ class EzPush
      * @param string $_subscriber
      * @param Message $_message
      */
-    public function Push($_bundleid,$_subscriber,$_message){
+    public static function Push($_message,$_subscriber,$_bundleid){
 
     }
 }
